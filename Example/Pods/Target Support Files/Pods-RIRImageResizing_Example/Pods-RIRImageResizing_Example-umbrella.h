fd0a437468f333fde52488674e31c11677c95562
Benjamin Maer <ben@resplendent.co>
2016-10-07 14:42:35 -0700

    Removed unused pod, installed example pod project

---
+++ Example/Pods/Target Support Files/Pods-RIRImageResizing_Example/Pods-RIRImageResizing_Example-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_RIRImageResizing_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_RIRImageResizing_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/RIRImageResizing/RIRImageResizing-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RIRImageResizing : NSObject
+@end
+@implementation PodsDummy_RIRImageResizing
+@end
